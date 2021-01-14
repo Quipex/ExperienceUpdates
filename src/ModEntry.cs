32da289c076e56daa3c4bc3cfe2ea7ff65ecbb6b
Quipex <rudas72@gmail.com>
2021-01-15 00:37:18 +0200

    Added GenericModConfigMenu support

--- src/ModEntry.cs
+++ src/ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using System;
 
 namespace ExperienceUpdates
 {
@@ -14,9 +15,22 @@ namespace ExperienceUpdates
         {
             Config = helper.ReadConfig<Configuration>();
 
-            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
-            helper.Events.GameLoop.ReturnedToTitle += this.OnReturnedToTitle;
-            helper.Events.Display.RenderedHud += this.OnRenderedHud;
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            helper.Events.GameLoop.ReturnedToTitle += OnReturnedToTitle;
+            helper.Events.Display.RenderedHud += OnRenderedHud;
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            var capi = Helper.ModRegistry.GetApi<GenericModConfigMenuAPI>("spacechase0.GenericModConfigMenu");
+            if (capi != null)
+            {
+                capi.RegisterModConfig(ModManifest, () => Config = new Configuration(), () => Helper.WriteConfig(Config));
+                capi.RegisterSimpleOption(ModManifest, "X coordinate", "Pixels from the left side of the screen if positive. From the right side if negative", () => Config.X, (int val) => Config.X = val);
+                capi.RegisterSimpleOption(ModManifest, "Y coordinate", "Pixels from the top of the screen if positive. From the bottom of the screen if negative", () => Config.Y, (int val) => Config.Y = val);
+                capi.RegisterSimpleOption(ModManifest, "Animation duration", "Time in milliseconds for the numbers animation to perform. 4000 is 4 seconds", () => Config.TextDurationMS, (int val) => Config.TextDurationMS = val);
+            }
         }
 
         private void OnReturnedToTitle(object sender, ReturnedToTitleEventArgs e)
